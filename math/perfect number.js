405c9f2651fc106a1cc67df52803efa72d084b29
xbloo <junecug@gmail.com>
2020-01-30 23:50:53 -0500

    update more

---
+++ math/perfect number.js
@@ -0,0 +1,22 @@
+Example : The first perfect number is 6, because 1, 2, and 3 are its proper positive divisors, and 1 + 2 + 3 = 6. Equivalently, the number 6 is equal to half the sum of all its positive divisors: ( 1 + 2 + 3 + 6 ) / 2 = 6. The next perfect number is 28 = 1 + 2 + 4 + 7 + 14. This is followed by the perfect numbers 496 and 8128.
+function is_perfect(number)
+{
+var temp = 0;
+   for(var i=1;i<=number/2;i++)
+     {
+         if(number%i === 0)
+          {
+            temp += i;
+          }
+     }
+   
+     if(temp === number && temp !== 0)
+        {
+       console.log("It is a perfect number.");
+        } 
+     else
+        {
+       console.log("It is not a perfect number.");
+        }   
+ } 
+is_perfect(28); 
---
+++ math/用filter的.js
@@ -0,0 +1,11 @@
+1.筛选大于某个值的
+
+function BiggerElements(val)
+   {
+     return function(evalue, index, array)
+     {
+     return (evalue >= val);
+     };
+   }
+var result = [11, 45, 4, 31, 64, 10]. filter(BiggerElements(10));
+console.log(result);
